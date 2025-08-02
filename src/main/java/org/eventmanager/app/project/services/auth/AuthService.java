73158a1270b3bcedf96a89c5e323f0e62eab5d28
DaniloLO53 <daniloleaodev@gmail.com>
2025-08-02 14:52:23 -0300

    [feat] sign out user

--- src/main/java/org/eventmanager/app/project/services/auth/AuthService.java
+++ src/main/java/org/eventmanager/app/project/services/auth/AuthService.java
@@ -6,9 +6,11 @@ import org.eventmanager.app.project.payload.request.auth.SignUpRequestPayload;
 import org.eventmanager.app.project.payload.response.auth.SignInResponsePayload;
 import org.eventmanager.app.project.payload.response.auth.UserInfoPayload;
 import org.eventmanager.app.project.security.services.UserDetailsImpl;
+import org.springframework.http.ResponseCookie;
 
 public interface AuthService {
     void signUp(SignUpRequestPayload payload);
     SignInResponsePayload signIn(@Valid SignInRequestPayload payload);
     UserInfoPayload getUserInfoByUserDetails(UserDetailsImpl userDetails);
+    ResponseCookie getCleanJwtCookie();
 }
